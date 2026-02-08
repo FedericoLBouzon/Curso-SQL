6be644ceb38066e3b69293da8beb4fd0350c46a1
Federico Leonel Bouzón <federicolbouzon@gmail.com>
2026-02-08 13:23:47 -0300

    Insertar primeros registros SQL #3
    LINK DEL VIDEO:
    https://youtu.be/Z60KWC_glLM

---
+++ Insertar registros en SQL.sql
@@ -0,0 +1,5 @@
+Use TestFede;
+Insert into Empleados(Nombre,Apellido,DNI) -- Inser into NOMBRETABLA Y (los campos) 
+values ('Pedro','Arbusto',10304880); -- Values (Los datos del Empleado)
+
+Select * from Empleados; -- Seleccione toda la tabla de empleados para verificar que se haya cargado correctamente
