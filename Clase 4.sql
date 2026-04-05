9e1c5a18f11aa10ec1c08ac83e0ca1d1a9c0f973
Federico Leonel Bouzón <federicolbouzon@gmail.com>
2026-04-05 14:15:20 -0300

    Primer Where

---
+++ Clase 4.sql
@@ -0,0 +1,8 @@
+Use testFede;
+Select Nombre,DNI from empleados;
+
+Select *
+From empleados
+where Nombre ='peDro' and Apellido='arBusto'
+
+-- nombre por ejemo: Pasa todo a miniscula, es decir yo tengo "PEDRO" lo pasa a "pedro", si yo tengo "PeDRro" lo pasa a "pedro"
